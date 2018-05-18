27ca2e61de0babc39453373504eba967c824f183
Vladimir E. Koltunov <PROGrand@users.noreply.github.com>
2018-05-18 14:38:35 +0300

    Update YPFiltersView.swift

--- Source/Filters/YPFiltersView.swift
+++ Source/Filters/YPFiltersView.swift
@@ -47,7 +47,7 @@ class YPFiltersView: UIView {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         layout.minimumLineSpacing = 4
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 18, bottom: 0, right: 18)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         layout.itemSize = CGSize(width: 100, height: 120)
         return layout
     }
